991bcf13f1c2b4a5e49940ec45e4c268cb1667ac
joshyaffee <jay1@rice.edu>
2023-12-10 14:53:31 -0600

    implemented Z(G) as good as we can

--- src/Invariants/zero_forcing/minimum_zero_forcing_set.jl
+++ src/Invariants/zero_forcing/minimum_zero_forcing_set.jl
@@ -1,5 +1,5 @@
 """
-    compute(::Type{MinimumZeroForcingSet}, g::AbstractGraph)
+    compute(::Type{BruteForceMinimumZeroForcingSet}, g::AbstractGraph)
 
 Return a minimum zero forcing set of `g` using a brute force method.
 
@@ -47,13 +47,14 @@ MinimumZeroForcingSet(Set{Int64}([1, 2, 3, 4]))
 """
 function compute(
     ::Type{MinimumZeroForcingSet},
-    g::SimpleGraph{T}
+    g::SimpleGraph{T};
+    optimizer= HiGHS.Optimizer
 ) where T <: Integer
 
     # convert to directed graph, replacing each edge with two directed edges
     directed_g = Graphs.DiGraph(g)
 
-    return compute(MinimumZeroForcingSet, directed_g)
+    return compute(MinimumZeroForcingSet, directed_g, optimizer)
 end
 
 """
@@ -82,47 +83,43 @@ MinimumZeroForcingSet(Set{Int64}([1, 2, 3, 4]))
 function compute(
     ::Type{MinimumZeroForcingSet},
     g::DiGraph{T},
-    optimizer=HiGHS.Optimizer
+    optimizer= HiGHS.Optimizer
 ) where T <: Integer
 
     # Instantiate the model.
     model = Model(optimizer)
     JuMP.set_silent(model)
 
-    # The number of vertices.
-    n = Graphs.nv(g)
+    node_array = collect(Graphs.vertices(g))
+    edge_array = [(e.src, e.dst) for e in collect(Graphs.edges(g))]
 
-    # The number of directed edges.
-    m = Graphs.ne(g)
-
-    max_steps = n   # very loose bound for now; should be tightened 
-                    # (to diameter of graph?) but I leave it like this for now
+    max_steps = length(node_array) - 1 # very loose bound for now; should be tightened 
 
     # Binary decision variable for each vertex - is it in Z(G)?
-    @variable(model, s[1:n], Bin)
+    @variable(model, s[1:length(node_array)], Bin)
 
     # Integer decision variable for each vertex - how many steps does it take to force it?
-    @variable(model, x[1:n], Int, lower_bound = 0, upper_bound = max_steps)
+    @variable(model, x[1:length(node_array)], Int, lower_bound = 0, upper_bound = max_steps)
 
     # Binary decision variable for each directed edge (u,v) - does u force v?
-    @variable(model, y[1:m], Bin)
+    @variable(model, y[edge_array], Bin)
 
     # Objective: Minimize the number of vertices in Z(G)
-    @objective(model, Min, sum(s[i] for i in 1:n))
+    @objective(model, Min, sum(s))
 
     # Constraints: For every vertex, v, it's in the set or exactly on edge is forcing it
-    for (v, node) in enumerate(Graphs.vertices(g))
+    for node in node_array
         # find index of all edges that have node as their destination
         # delta_minus = findall(a -> Graphs.dst(a) == node, Graphs.edges(g)) # this is buggy
         delta_minus = []
-        for (e, edge) in enumerate(edges(g))
-            if Graphs.dst(edge) == node
-                push!(delta_minus, e)
+        for edge in edge_array
+            if edge[2] == node
+                push!(delta_minus, edge)
             end
         end
         
         # add constraint
-        @constraint(model, s[v] + sum(y[e] for e in delta_minus) == 1.0)
+        @constraint(model, s[node] + sum(y[edge] for edge in delta_minus) == 1.0)
     end
 
     # Constraints: For every directed edge:
@@ -130,23 +127,18 @@ function compute(
     # and
     # For every directed edge, (u,v), For all neighbors of u that are not v, call them w:
         # x_w - x_v + (max_steps + 1) * y_uv <= max_steps
-    for (e, edge) in enumerate(edges(g))
-        u, v = Graphs.src(edge), Graphs.dst(edge)
-
-        # lambda functions used in case == is ever overridden
-        index_u = findfirst(a -> a == u, vertices(g)) 
-        index_v = findfirst(a -> a == v, vertices(g))
+    for edge in edge_array
+        u, v = edge[1], edge[2]
 
         # add constraint 1
-        @constraint(model, x[index_u] - x[index_v] + (max_steps + 1) * y[e] <= max_steps)
+        @constraint(model, x[u] - x[v] + (max_steps + 1) * y[edge] <= max_steps)
 
         # find all neighbors of u that are not v
-        for w in Graphs.outneighbors(g, u)
+        for w in Graphs.inneighbors(g, u)
             if w != v
-                index_w = findfirst(a -> a == w, vertices(g))
 
                 # add constraint 2
-                @constraint(model, x[index_w] - x[index_v] + (max_steps + 1) * y[e] <= max_steps)
+                @constraint(model, x[w] - x[v] + (max_steps + 1) * y[edge] <= max_steps)
             end
         end
     end
@@ -155,7 +147,93 @@ function compute(
     optimize!(model)
 
     # Extract the solution.
-    zero_forcing_set = [Graphs.vertices(g)[v] for v in 1:n if value(s[v]) == 1.0]
+    zero_forcing_set = [v for v in node_array if value(s[v]) == 1.0]
 
     return MinimumZeroForcingSet(zero_forcing_set)
 end
+
+# function compute(
+#     ::Type{MinimumZeroForcingSet},
+#     g::DiGraph{T},
+#     optimizer= HiGHS.Optimizer
+# ) where T <: Integer
+
+#     # Instantiate the model.
+#     model = Model(optimizer)
+#     JuMP.set_silent(model)
+
+#     # The number of vertices.
+#     n = Graphs.nv(g)
+
+#     # The number of directed edges.
+#     m = Graphs.ne(g)
+
+#     max_steps = n   # very loose bound for now; should be tightened 
+#                     # (to diameter of graph?) but I leave it like this for now
+    
+#     node_array = collect(Graphs.vertices(g))
+#     edge_array = collect(Graphs.edges(g))
+
+#     # Binary decision variable for each vertex - is it in Z(G)?
+#     @variable(model, s[1:n], Bin)
+
+#     # Integer decision variable for each vertex - how many steps does it take to force it?
+#     @variable(model, x[1:n], Int, lower_bound = 0, upper_bound = max_steps)
+
+#     # Binary decision variable for each directed edge (u,v) - does u force v?
+#     @variable(model, y[1:m], Bin)
+
+#     # Objective: Minimize the number of vertices in Z(G)
+#     @objective(model, Min, sum(s[i] for i in 1:n))
+
+#     # Constraints: For every vertex, v, it's in the set or exactly on edge is forcing it
+#     for (v, node) in enumerate(node_array)
+#         # find index of all edges that have node as their destination
+#         # delta_minus = findall(a -> Graphs.dst(a) == node, Graphs.edges(g)) # this is buggy
+#         delta_minus = []
+#         for (e, edge) in enumerate(edge_array)
+#             if Graphs.dst(edge) == node
+#                 push!(delta_minus, e)
+#             end
+#         end
+        
+#         # add constraint
+#         @constraint(model, s[v] + sum(y[e] for e in delta_minus) == 1.0)
+#     end
+
+#     # Constraints: For every directed edge:
+#         #(u,v), x_u - x_v + (max_steps + 1) * y_uv <= max_steps
+#     # and
+#     # For every directed edge, (u,v), For all neighbors of u that are not v, call them w:
+#         # x_w - x_v + (max_steps + 1) * y_uv <= max_steps
+#     for (e, edge) in enumerate(edge_array)
+#         u, v = Graphs.src(edge), Graphs.dst(edge)
+
+#         # lambda functions used in case == is ever overridden
+#         index_u = findfirst(a -> a == u, node_array) 
+#         index_v = findfirst(a -> a == v, node_array)
+
+#         # add constraint 1
+#         @constraint(model, x[index_u] - x[index_v] + (max_steps + 1) * y[e] <= max_steps)
+
+#         # find all neighbors of u that are not v
+#         for w in Graphs.inneighbors(g, u)
+#             if w != v
+#                 index_w = findfirst(a -> a == w, node_array)
+
+#                 # add constraint 2
+#                 @constraint(model, x[index_w] - x[index_v] + (max_steps + 1) * y[e] <= max_steps)
+#             end
+#         end
+#     end
+
+#     # Solve the model.
+#     optimize!(model)
+
+#     # Extract the solution.
+#     zero_forcing_set = [node_array[v] for v in 1:n if value(s[v]) == 1.0]
+
+#     return MinimumZeroForcingSet(zero_forcing_set)
+# end
+
+#######################
