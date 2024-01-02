9f8c16a8d4a4608dec915749754b692857ffc8cc
joshyaffee <jay1@rice.edu>
2024-01-02 16:45:42 -0600

    added documentation
    Also uncommented other tests in runtests.jl

--- src/Communities/algorithms/pagerank.jl
+++ src/Communities/algorithms/pagerank.jl
@@ -1,15 +1,17 @@
 """
     compute(algo::PageRank, g::AbstractGraph)::Vector{Float64}
 
-Compute the PageRank values of the nodes in graph `g` using the PageRank algorithm.
+Compute the PageRank values of the nodes in graph `g` using the specified PageRank algorithm.
 
 # Arguments
 
 - `algo::PageRank`: The PageRank algorithm configuration object. This should contain properties like
-  (initial) damping factor (`d`), maximum number of iterations (`max_iter`), tolerance (`tol`), and .
+  (initial) damping factor (`d`), maximum number of iterations (`max_iter`), tolerance (`tol`), and the method:
+  "classical", "iterative", or "adaptive" (method_name). The two latter methods are described in the paper
+  PageRank: New Regularizations and Simulation Models (2011) by Boris T. Polyak and Anna V. Timonina
 
-- `g::AbstractGraph`: The graph for which to compute the PageRank. This can be a simple graph, directed
-  graph, or a weighted version of these.
+- `g::AbstractGraph`: The graph for which to compute the PageRank. This can be a simple graph, directed or
+  undirected, or a weighted version of these.
 
 # Returns
 
@@ -17,63 +19,20 @@ Compute the PageRank values of the nodes in graph `g` using the PageRank algorit
 
 # Details
 
-The function uses the power iteration method to compute the PageRank values. If the graph is weighted,
-the weights of the edges are taken into account while calculating the rank.
-
-The algorithm iteratively refines the PageRank values until either the maximum number of iterations
-is reached or the values converge within the specified tolerance.
-
-# Example
+The function uses the power iteration method to find the principal eigenvector of an altered adjacency matrix.
+The altered matrix is created by 'damping' the original matrix, which is done by adding a constant to each entry
+as well as taking care of sinks. The iterative and adaptive methods increase the damping factor (decreases perturbance of P) 
+as the algorithm progresses, which is shown to improve convergence. The iterative method increases the damping factor
+over a determined sequence, while the adaptive method maintains the damping factor until a relaxed tolerance is reached, 
+then lowers the tolerance and repeats until the original tolerance is reached.
 
 ```julia
 julia> g = generate(PlantedPartition())
 
-julia> algo = PageRank(d=0.85, max_iter=100, tol=1e-6, method_name=")
+julia> algo = PageRank(d=0.85, max_iter=100, tol=1e-6, method_name="adaptive")
 
 julia> compute(algo, g)
-```
 """
-# function compute(algo::PageRank, g::AbstractGraph)
-
-#     N = nv(g)
-#     PR = fill(1.0 / N, N)  # Initial rank
-#     old_PR = copy(PR)
-#     is_weighted = isa(g, SimpleWeightedGraph) || isa(g, SimpleWeightedDiGraph)
-
-#     # Out-Degree Weights
-#     W = Vector{Float64}(undef, N)
-#     for i in 1:N
-#         if is_weighted
-#             W[i] = sum(outneighbors(g, i) .|> out_vertex -> get_weight(g, i, out_vertex))
-#         else
-#             W[i] = outdegree(g, i)
-#         end
-#     end
-
-#     for _ in 1:algo.max_iter
-#         for i in 1:N
-#             s = 0.0
-#             for j in inneighbors(g, i)
-#                 wij = is_weighted ? get_weight(g, j, i) : 1.0
-#                 s += wij * PR[j] / W[j]
-#             end
-#             PR[i] = (1 - algo.d) + algo.d * s
-#         end
-
-#         # Check for convergence
-#         if maximum(abs.(PR - old_PR)) < algo.tol
-#             break
-#         end
-#         old_PR = copy(PR)
-#     end
-
-#     # Normalize
-#     PR ./= sum(PR)
-
-#     return PR
-# end
-
-
 function compute(
     algo::PageRank,
     g::AbstractGraph
@@ -111,6 +70,22 @@ function compute(
     return pagerank_function(P, d, tol, max_iter)
 end  
 
+"""
+    _classical_pagerank(P::SparseMatrixCSC, d::Float64, tol::Float64, max_iter::Int64)::Vector{Float64}
+
+Helper function for compute(Pagerank, g), computes the PageRank values of the nodes in graph `g` using the classical PageRank algorithm.
+
+# Arguments
+
+- `P::SparseMatrixCSC`: The Markov Chain associated with `g`.
+- `d::Float64`: The damping factor.
+- `tol::Float64`: The tolerance for convergence.
+- `max_iter::Int64`: The maximum number of iterations.
+
+# Returns
+
+- A vector of `Float64` where each entry represents the PageRank value of the corresponding node in the graph.
+"""
 function _classical_pagerank(
     P::SparseMatrixCSC, 
     d::Float64, 
@@ -137,6 +112,22 @@ function _classical_pagerank(
     return pr
 end
 
+"""
+    _iterative_pagerank(P::SparseMatrixCSC, d::Float64, tol::Float64, max_iter::Int64)::Vector{Float64}
+
+Helper function for compute(Pagerank, g), computes the PageRank values of the nodes in graph `g` using the iterative regularization PageRank algorithm.
+
+# Arguments
+
+- `P::SparseMatrixCSC`: The Markov Chain associated with `g`.
+- `d::Float64`: The initial damping factor, from which the regularization factor is computed. 0 < d < .5
+- `tol::Float64`: The tolerance for convergence.
+- `max_iter::Int64`: The maximum number of iterations.
+
+# Returns
+
+- A vector of `Float64` where each entry represents the PageRank value of the corresponding node in the graph.
+"""
 function _iterative_regularization_pagerank(
     P::SparseMatrixCSC, 
     d::Float64, 
@@ -170,6 +161,22 @@ function _iterative_regularization_pagerank(
     return pr
 end
 
+"""
+    _adaptive_pagerank(P::SparseMatrixCSC, d::Float64, tol::Float64, max_iter::Int64)::Vector{Float64}
+
+Helper function for compute(Pagerank, g), computes the PageRank values of the nodes in graph `g` using the adaptive regularization PageRank algorithm.
+
+# Arguments
+
+- `P::SparseMatrixCSC`: The Markov Chain associated with `g`.
+- `d::Float64`: The initial damping factor. 0 < d < 1.0
+- `tol::Float64`: The tolerance for convergence.
+- `max_iter::Int64`: The maximum number of iterations.
+
+# Returns
+
+- A vector of `Float64` where each entry represents the PageRank value of the corresponding node in the graph.
+"""
 function _adaptive_regularization_pagerank(
     P::SparseMatrixCSC, 
     d::Float64, 
